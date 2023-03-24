d1004637226229383a9b6c95b254aae696602f59
wincent <3511606256@qq.com>
2023-03-24 21:37:27 +0800

    build

---
+++ build/CMakeFiles/Single_OpticalFlow.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wincent/Tiny_Visual_Odometry/Single_OpticalFlow.cpp" "/home/wincent/Tiny_Visual_Odometry/build/CMakeFiles/Single_OpticalFlow.dir/Single_OpticalFlow.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ENABLE_SSE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/Eigen"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Single_OpticalFlow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Single_OpticalFlow.dir/Single_OpticalFlow.cpp.o"
+  "Single_OpticalFlow"
+  "Single_OpticalFlow.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Single_OpticalFlow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
